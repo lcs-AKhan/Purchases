7da143d1becc35b54256c0e2251cb24af529b234
Abdul Khan <akhan@lcs.on.ca>
2020-11-04 10:39:33 -0500

    Initial Commit

---
+++ Purchases/PurchasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurchasesApp.swift
+//  Purchases
+//
+//  Created by Abdul Ahad Khan on 2020-11-04.
+//
+
+import SwiftUI
+
+@main
+struct PurchasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
